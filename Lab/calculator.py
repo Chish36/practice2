73b4710dc13a9ef73387d3da931ee9fa07c61086
Chish36 <b.chisholm2002@gmail.com>
2024-12-11 11:43:51 +0000

    Added a doc string

--- Lab/calculator.py
+++ Lab/calculator.py
@@ -4,6 +4,11 @@ class calculator:
         self.name = name
 
     def addition(self, int1, int2):
+        '''Adds two integers together
+        
+        input: int1 and int2 must be integers
+        returns: Sum of int1 and int2
+        '''
         print(f"Answer is {int1 + int2}")
 
     def subtraction(self, int1, int2):
